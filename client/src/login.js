d0ce4e3b7692fa1307d0bf4661d9ef4d730b2352
mpark63 <minspk13@gmail.com>
2023-12-13 11:33:48 -0800

    edit login.js

--- client/src/login.js
+++ client/src/login.js
@@ -1,14 +1,12 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
 
 const Login = (props) => {
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
     const [usernameError, setUsernameError] = useState("")
     const [passwordError, setPasswordError] = useState("")
+    const [dangerousInputs, setDangerousInputs] = useState([])
     
-    const navigate = useNavigate();
-        
     const onButtonClick = () => {
 
         // Set initial error values to empty
@@ -26,47 +24,35 @@ const Login = (props) => {
             return
         }
 
-        // Check if username has an account associated with it
-        checkAccountExists(username)
-            .then(accountExists => {
-                if (accountExists) {
-                    logIn();
-                } else {
-                    window.alert("An account does not exist with this username address: " + username)
-                }
-            })
-            .catch(error => {
-                console.error('Error checking account:', error);
-                // Handle the error, show a message to the user, or perform other actions
-            });
-    }
+        logIn();
 
-    // Call the server API to check if the given username ID already exists
-    const checkAccountExists = (username) => {
-        return fetch("http://localhost:5001/check-account", {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ username })
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            return data.userExists; // Assuming the response contains a boolean indicating if the user exists
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            throw new Error('Failed to check account');
-        });
-    };
+        // // Check if username has an account associated with it
+        // checkAccountExists(username)
+        //     .then(accountExists => {
+        //         if (accountExists) {
+        //             logIn();
+        //         } else {
+        //             window.alert("An account does not exist with this username address: " + username)
+        //         }
+        //     })
+        //     .catch(error => {
+        //         console.error('Error checking account:', error);
+        //         // Handle the error, show a message to the user, or perform other actions
+        //     });
+    }
 
     // Log in a user using username and password
     const logIn = () => {
+        // keyword detection 
+        if (keywordDetection(username, password)) {
+            console.log('keyword detected before gpt call')
+            return window.alert("Possible NoSQL attack detected");
+        }
+        // lookup in cache 
+        if (dangerousInputs.includes(username) || dangerousInputs.includes(password)) {
+            console.log('dangerous input detected in cache before gpt call')
+            return window.alert("Possible NoSQL attack detected");
+        }
         fetch("http://localhost:5001/login", {
             method: "POST",
             headers: {
@@ -75,22 +61,31 @@ const Login = (props) => {
             body: JSON.stringify({ username, password })
         })
         .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            if (data.message === "Invalid username or password") {
+            if (response.status === 403) {
+                setDangerousInputs([...dangerousInputs, username, password])
+                window.alert("Possible NoSQL attack detected");
+            } else if (response.status === 404) {
                 window.alert("Invalid username or password");
-            } else {
+            } else if (response.status === 200) {
                 window.alert("Logged in as " + username);
+            } else {
+                window.alert("Something went wrong");
             }
         })
         .catch(error => {
-            window.alert("Failed to log in");
+            window.alert("Something went wrong");
         });
     };
+
+    const keywordDetection = (username, password) => {
+        const keywords = ['$where', '$regex', '$ne']
+        for (let i = 0; i < keywords.length; i++) {
+            if (username.includes(keywords[i]) || password.includes(keywords[i])) {
+                return true; 
+            }
+        }
+        return false;
+    }
     
 
     return <div className={"mainContainer"}>
